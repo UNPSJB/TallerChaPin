0d2560c96a066443c95ed05698f4901dbf1c115e
Matias Barea <mbarea@rw.dgrchubut.gov.ar>
2021-09-23 12:16:05 -0300

    mas cambios a los templates, filtro en listar empleados agregado

--- TallerChaPin/taller/urls.py
+++ TallerChaPin/taller/urls.py
@@ -19,10 +19,10 @@ from .views import (
 urlpatterns = [
     # path('', index),
     # path('marcas/', listado_marcas),
-    path('marcas/crear', MarcaCreateView.as_view(), name="crearMarca"),
-    path('marcas/listar', MarcaListView.as_view(), name="listarMarcas"),
     #path('marcas/<slug:marca>/modelos', modelos_x_marcas, name="modelosXmarcas"),
     #path('cliente/registrar', registrar_cliente, name="registrarCliente"),
+    path('marcas/crear', MarcaCreateView.as_view(), name="crearMarca"),
+    path('marcas/listar', MarcaListView.as_view(), name="listarMarcas"),  
     path('modelos/listar', ModeloListView.as_view(), name="listarModelos"),
     path ('empleado/crear', EmpleadoCreateView.as_view(), name="crearEmpleado"),
     path('empleado/modificar/<int:pk>', EmpleadoUpdateView.as_view(), name="modificarEmpleado"),

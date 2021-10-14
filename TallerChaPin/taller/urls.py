ba4f9d2ae5c43fa329ccc4add045ed5f3de6da17
= <ramirocanario1@gmail.com>
2021-10-14 16:51:26 -0300

    Ordené urls.py

--- TallerChaPin/taller/urls.py
+++ TallerChaPin/taller/urls.py
@@ -26,23 +26,32 @@ from .views import (
 
 
 urlpatterns = [
+    # ----------------- MARCAS -----------------
     path('marcas/crear', MarcaCreateView.as_view(), name="crearMarca"),
+    path('marcas/listar', MarcaListView.as_view(), name="listarMarcas"), 
+    path('marca/modificar/<int:pk>', MarcaUpdateView.as_view(), name="modificarMarca"),
+    path('marca/eliminar/<int:pk>', MarcaDeleteView.as_view(), name="eliminarMarca"),
+    # ----------------- TAREAS -----------------
     path('tarea/crear', TareaCreateView.as_view(), name="crearTarea"),
+    path('tareas/listar', TareaListView.as_view(), name="listarTareas"),
+    # ----------------- TIPOS TAREA -----------------
     path('tipo-tarea/crear', TipoTareaCreateView.as_view(), name="crearTipoTarea"),
+    # ----------------- REPUESTOS -----------------
     path('repuesto/crear', RepuestoCreateView.as_view(), name="crearRepuesto"),
-    path('marcas/listar', MarcaListView.as_view(), name="listarMarcas"),  
+    path('repuestos/listar', RepuestoListView.as_view(), name="listarRepuestos"),
+     # ----------------- MODELO -----------------
     path('modelo/crear', ModeloCreateView.as_view(), name="crearModelo"),
     path('modelos/listar', ModeloListView.as_view(), name="listarModelos"),
-    path('repuestos/listar', RepuestoListView.as_view(), name="listarRepuestos"),
-    path('tareas/listar', TareaListView.as_view(), name="listarTareas"),
+    # ----------------- MATERIALES -----------------
     path('materiales/listar', MaterialListView.as_view(), name="listarMateriales"),
+    # ----------------- CLIENTES -----------------
     path('clientes/listar', ClienteListView.as_view(), name='listarClientes'),
     path('clientes/crear', ClienteCreateView.as_view(), name='crearCliente'),
+    # ----------------- VEHICULOS -----------------
     path('vehiculos/listar', VehiculoListView.as_view(), name="listarVehiculos"),
+    # ----------------- EMPLEADOS -----------------    
     path ('empleado/crear', EmpleadoCreateView.as_view(), name="crearEmpleado"),
     path('empleado/modificar/<int:pk>', EmpleadoUpdateView.as_view(), name="modificarEmpleado"),
     path('empleado/eliminar/<int:pk>', EmpleadoDeleteView.as_view(), name="eliminarEmpleado"),
     path ('empleados/listar', EmpleadoListView.as_view(), name="listarEmpleados"),
-    path('marca/modificar/<int:pk>', MarcaUpdateView.as_view(), name="modificarMarca"),
-    path('marca/eliminar/<int:pk>', MarcaDeleteView.as_view(), name="eliminarMarca"),
 ]

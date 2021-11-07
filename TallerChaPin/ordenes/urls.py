247222f3602128326f52ee73277605ed9a61f381
matias <matyaseze777@gmail.com>
2021-11-07 15:49:15 -0300

    algunos cambios, templates,urls de orden de trabajo agregados y faltan vistas y forms

--- TallerChaPin/ordenes/urls.py
+++ TallerChaPin/ordenes/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
          PresupuestoUpdateView.as_view(), name="modificarPresupuesto"),
     path('presupuesto/eliminar/<int:pk>',
          PresupuestoDeleteView.as_view(), name="eliminarPresupuesto"),
+     # ----------------- Presupuesto -----------------
+    path('orden/crear', PresupuestoCreateView.as_view(), name="crearPresupuesto"), #No se si haga falta esta vista, supongo que del lado del sistema se genera sola la orden de trabajo
+    path('orden/listar', PresupuestoListView.as_view(), name="listarPresupuestos"),
+    path('orden/detalle/<int:pk>',
+         PresupuestoDetailView.as_view(), name="detallesOrden"),
+    path('orden/modificar/<int:pk>',
+         PresupuestoUpdateView.as_view(), name="modificarOrden"),
+    path('orden/eliminar/<int:pk>',
+         PresupuestoDeleteView.as_view(), name="eliminarOrden"), # No se elimina como tal, se cambia el estado a cancelado
 
 ]

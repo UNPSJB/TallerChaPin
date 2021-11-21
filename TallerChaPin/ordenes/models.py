76c8853bdb8b0dd55ef60160e36d8fc58d8f9d71
= <ramirocanario1@gmail.com>
2021-11-21 19:19:06 -0300

    Correcciones varias a templates

--- TallerChaPin/ordenes/models.py
+++ TallerChaPin/ordenes/models.py
@@ -237,7 +237,7 @@ class DetalleOrdenDeTrabajo(models.Model):
         return cls(tarea=tarea)
 
     def iniciar(self, empleado, fecha=now()):
-        self.empleado = empleado
+        #self.empleado = empleado       El empleado se asigna con el método asignar(). Hace falta acá también?
         self.inicio = fecha
         self.save()
 

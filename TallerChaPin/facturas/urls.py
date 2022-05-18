4d14fb7a5254dd4ee6ac69161bf6b8551eadd87e
matias <matyaseze777@gmail.com>
2022-05-18 10:49:45 -0300

    Cambios en urls

--- TallerChaPin/facturas/urls.py
+++ TallerChaPin/facturas/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
          FacturaDeleteView.as_view(), name="eliminarFactura"),
     # ----------------Pago-------------------------
     path('pagos/crear/<int:pk>', PagoCreateView.as_view(), name="crearPago"),
-    path('pagos/crear/<int:pk>/debito', PagoDebitoView.as_view(), name="pagoDebito"),
+    path('pagos/crear/debito/<int:pk>', PagoDebitoView.as_view(), name="pagoDebito"),
     path('pagos/crear/<int:pk>/credito', PagoCreditoView.as_view(), name="pagoCredito"),
     path('pagos/crear/<int:pk>/contado', PagoContadoView.as_view(), name="pagoContado"),
     path('pagos/detalle/<int:pk>', PagoDetailView.as_view(), name="detallesPago"),

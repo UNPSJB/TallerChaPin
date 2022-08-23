e3d1d9af03fd807aa4308ff23d2934b9c81c6599
Bruno <pazosbruno@gmail.com>
2022-08-23 20:39:32 -0300

    obteniendo el target correcto (btn)

--- TallerChaPin/static/js/modals.js
+++ TallerChaPin/static/js/modals.js
@@ -1,17 +1,16 @@
-const mostrarModalEliminacion = (modal_id) => {
+const mostrarModalEliminacion = (e, modal_id) => {
   /* 
     Configura un modal de eliminación con un mensaje en el cuerpo del mismo
     y una URL de navegación para el botón de confirmación.
   */
   const modal_tag = document.getElementById(modal_id);
-
   // ocultamos el modal para poder configurarlo
   const modal_instance = bootstrap.Modal.getInstance(modal_tag);
   modal_instance.hide();
 
   // configuramos el contenido del modal
-  modal_tag.addEventListener('show.bs.modal', (e) => {
-    const btn = e.explicitOriginalTarget;
+  modal_tag.addEventListener('show.bs.modal', () => {
+    const btn = e.target; // e.target es el boton al que se le hizo click.
     const url = btn.dataset.bsUrl;
     const nombre = btn.dataset.bsNombre;
     const msj = `¿Estás seguro/a de querer <strong>eliminar</strong> "${nombre}" ?`;
@@ -29,7 +28,7 @@ const mostrarModalEliminacion = (modal_id) => {
 const conectarBotonesAModal = (btn_selector, modal_id) => {
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll(btn_selector).forEach(btn => {
-      btn.addEventListener('click', () => mostrarModalEliminacion(modal_id));
+      btn.addEventListener('click', (e) => mostrarModalEliminacion(e, modal_id));
     })
   })
 }
